096c123cfede814ae83378d28c3f93b2e000a048
MarqCheddaBeans <isaiahmv27@yahoo.com>
2025-12-03 12:58:45 -0500

    Excercise 3 completed, user can search for products via inputted category id

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,8 +1,11 @@
 package com.pluralsight;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class App {
+
+    public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
 
@@ -19,44 +22,160 @@ public class App {
         String username = args[0];
         String password = args[1];
 
-        try(
-            //Create the connection(similar to opening mySQL workbench)
-            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);
 
-            PreparedStatement q = c.prepareStatement("""
-                    SELECT
-                        ProductName
-                    FROM
-                        Products               
-                    """);
-            ResultSet results = q.executeQuery();
-        ){
-            printResults(results);
+        try(Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password)){
+
+            while(true){
+
+                System.out.println("""
+                        What do you want to do?
+                            1) Display All Products
+                            2) Display All Customers
+                            3) Display All Categories
+                            0) Exit App                        
+                        """);
+
+                switch(scan.nextInt()){
+                    case(1):
+                        displayAllProducts(c);
+                        break;
+                    case(2):
+                        displayAllCustomers(c);
+                        break;
+                    case(3):
+                        displayAllCategories(c);
+                    case(0):
+                        System.out.println("Bye");
+                        System.exit(0);
+                    default:
+                        System.out.println("Try Again");
+                }
+            }
 
         }catch(SQLException e){
             System.out.println("Something went wrong" + e);
         }
     }
 
+    public static void displayAllCategories(Connection c){
+        try(
+
+                PreparedStatement q = c.prepareStatement("""
+                        SELECT 
+                            CategoryID
+                            ,CategoryName
+                        FROM 
+                            Categories
+                        """);
+
+                ResultSet result = q.executeQuery();
+                ){
+
+            printResults(result);
+
+        }catch(SQLException e){
+            System.out.println("Error with query");
+        }
+
+        System.out.println("Enter a CategoryID to view related products");
+        int catID = scan.nextInt();
+
+        displayCategoryProducts(c , catID);
+    }
+
+    public static void displayCategoryProducts(Connection c, int i){
+
+        try(
+                PreparedStatement q = c.prepareStatement("""
+                        SELECT 
+                            ProductID
+                            ,ProductName
+                            ,UnitPrice
+                            ,UnitsInStock
+                        FROM
+                            Products
+                        WHERE
+                            CategoryID = ?
+                        """);
+                ){
+
+            q.setInt(1, i);
+
+            try(
+                    ResultSet result = q.executeQuery()
+                    ){
+                System.out.println("Products in category " + i + ": ");
+                printResults(result);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error retrieving products from category " + i);
+        }
+
+    }
+    public static void displayAllProducts(Connection c){
+
+        try(
+
+                PreparedStatement q = c.prepareStatement("""
+                    
+                    SELECT
+                        ProductID
+                        ,ProductName
+                        ,UnitPrice
+                        ,UnitsInStock
+                    FROM
+                        Products
+                    
+                    """);
+
+                ResultSet results = q.executeQuery();
+        ){
+            printResults(results);
+        } catch (SQLException e) {
+            System.out.println("Could not get all the products");
+            System.exit(1);
+        }
+    }
+
+    public static void displayAllCustomers(Connection c){
+
+        try(
+                PreparedStatement q = c.prepareStatement("""
+                        
+                        SELECT *
+                        FROM Customers
+                        
+                        """);
+
+                    ResultSet results = q.executeQuery();
+                ){
+            printResults(results);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
     //This method will be used inside of the display methods to print results to the screen
     public static void printResults(ResultSet r) throws SQLException {
+
         //Get the meta data so we have access to the field names
         ResultSetMetaData metaData = r.getMetaData();
+
         //Get the number of rows returned
         int columnCount = metaData.getColumnCount();
 
         while(r.next()){
 
-            //loop over each column in the row and display the data
-            for(int i = 1; i <= columnCount; i++){
-                //Get the current column value
+            for(int i = 1; i<=columnCount; i++){
+                String columnName = metaData.getColumnName(i);
                 String value = r.getString(i);
-                //print out the column name and value
-                System.out.println(value);
+                System.out.println(columnName + ": " + value);
             }
 
-            //print empty line to make results display prettier
-            System.out.println();
+            System.out.println("------------------------------");
+
         }
 
     }
